06b6944cdc230943aab308f338124b7f65c97373
Liam Kennedy <mailpk3118@gmail.com>
2023-02-17 14:20:00 -0500

    Ability to read exact PWM values

--- pwm_test/pwm_test.cpp
+++ pwm_test/pwm_test.cpp
@@ -1,59 +1,24 @@
 #include <stdio.h>
+
 #include "pico/stdlib.h"
-#include "hardware/pwm.h"
-#include "hardware/clocks.h"
+#include "pico/time.h"
 #include "tusb.h"
+#include "hardware/pwm.h"
 
-// This example drives a PWM output at a range of duty cycles, and uses
-// another PWM slice in input mode to measure the duty cycle. You'll need to
-// connect these two pins with a jumper wire:
-const uint OUTPUT_PIN = 2;
-const uint MEASURE_PIN = 5;
+#include "PwmIn.h"
 
-float measure_duty_cycle(uint gpio) {
-    // Only the PWM B pins can be used as inputs.
-    assert(pwm_gpio_to_channel(gpio) == PWM_CHAN_B);
-    uint slice_num = pwm_gpio_to_slice_num(gpio);
+#define NUM_OF_PINS 2
 
-    // Count once for every 100 cycles the PWM B input is high
-    pwm_config cfg = pwm_get_default_config();
-    pwm_config_set_clkdiv_mode(&cfg, PWM_DIV_B_HIGH);
-    pwm_config_set_clkdiv(&cfg, 100);
-    pwm_init(slice_num, &cfg, false);
-    gpio_set_function(gpio, GPIO_FUNC_PWM);
-
-    pwm_set_enabled(slice_num, true);
-    sleep_ms(10);
-    pwm_set_enabled(slice_num, false);
-    float counting_rate = clock_get_hz(clk_sys) / 100;
-    float max_possible_count = counting_rate * 0.01;
-    printf("pwm_get_count number: %d, max_possible_count: %f\n",
-	   pwm_get_counter(slice_num), max_possible_count);
-    return pwm_get_counter(slice_num) / max_possible_count;
-}
-
-const float test_duty_cycles[] = {
-        0.f,
-        0.1f,
-        0.5f,
-        0.9f,
-        1.f
-};
-
-int main() {
+int main()
+{
+    // needed for printf
     stdio_init_all();
-    printf("\nPWM duty cycle measurement example\n");
+    printf("PwmIn on 4 pins\n");
 
-    // Configure PWM slice and set it running
-    const uint count_top = 1000;
-    pwm_config cfg = pwm_get_default_config();
-    pwm_config_set_wrap(&cfg, count_top);
-    pwm_init(pwm_gpio_to_slice_num(OUTPUT_PIN), &cfg, true);
+    // set PwmIn
+    uint pin_list[NUM_OF_PINS] = {14, 15};
+    PwmIn my_PwmIn(pin_list, NUM_OF_PINS);
 
-    // Note we aren't touching the other pin yet -- PWM pins are outputs by
-    // default, but change to inputs once the divider mode is changed from
-    // free-running. It's not wise to connect two outputs directly together!
-    gpio_set_function(OUTPUT_PIN, GPIO_FUNC_PWM);
 
     printf("waiting for usb host");
     while (!tud_cdc_connected()) {
@@ -62,14 +27,37 @@ int main() {
     }
     printf("connected to usb\n\n");
 
-    // For each of our test duty cycles, drive the output pin at that level,
-    // and read back the actual output duty cycle using the other pin. The two
-    // values should be very close!
-    for (int i = 0; i < count_of(test_duty_cycles); ++i) {
-        float output_duty_cycle = test_duty_cycles[i];
-        pwm_set_gpio_level(OUTPUT_PIN, output_duty_cycle * (count_top + 1));
-        float measured_duty_cycle = measure_duty_cycle(MEASURE_PIN);
-        printf("Output duty cycle = %.1f%%, measured input duty cycle = %.1f%%\n",
-               output_duty_cycle * 100.f, measured_duty_cycle * 100.f);
+    const uint pwm_pin = 12;
+    const uint pwm_pin_2 = 10;
+    pwm_config cfg = pwm_get_default_config();
+    pwm_init(pwm_gpio_to_slice_num(pwm_pin), &cfg, true);
+    pwm_init(pwm_gpio_to_slice_num(pwm_pin_2), &cfg, true);
+
+    gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
+    gpio_set_function(pwm_pin_2, GPIO_FUNC_PWM);
+
+    uint pwm1_slice = pwm_gpio_to_slice_num(pwm_pin);
+    uint pwm2_slice = pwm_gpio_to_slice_num(pwm_pin_2);
+    pwm_set_clkdiv_int_frac(pwm1_slice, 2, 0);
+    pwm_set_clkdiv_int_frac(pwm2_slice, 2, 0);
+    // pwm_set_wrap(pwm_gpio_to_slice_num(pwm_pin), 15);
+    // pwm_set_chan_level()
+
+    //const uint16_t pwm_levels[5] = {1923, 17394, 27, 5903, 58201};
+
+    pwm_set_gpio_level(pwm_pin, 34008);
+    pwm_set_gpio_level(pwm_pin_2, 2023);
+
+    while (true)
+    {
+        // adviced empty (for now) function of sdk
+        tight_loop_contents();
+
+        // translate pwm of input to output
+        uint16_t PW_0 = my_PwmIn.read_PW(0);
+        uint16_t PW_1 = my_PwmIn.read_PW(1);
+        printf("PW_0=%d PW_1=%d\n", PW_0, PW_1);
+        sleep_ms(100);
     }
 }
+
