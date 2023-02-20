0efd68831fd31b85095b37adccd4c40a980e52b6
wevfreeman <wevfreeman@gmail.com>
2023-02-20 15:02:43 -0500

    moved report id defs to report_descriptor.h

--- usb_comms_alt/src/usb_descriptors.h
+++ usb_comms_alt/src/usb_descriptors.h
@@ -29,15 +29,6 @@
 #include "device/usbd.h"
 #include "tusb.h"
 
-enum
-{
-  REPORT_ID_KEYBOARD = 1,
-  REPORT_ID_MOUSE,
-  REPORT_ID_CONSUMER_CONTROL,
-  REPORT_ID_GAMEPAD,
-  REPORT_ID_COUNT
-};
-
 /*********************
  * STUFF TO DETERMINE
 **********************/
