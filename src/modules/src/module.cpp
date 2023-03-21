ee126e9aa1b4a1bb4b277b499986a40bb4c80f6d
nataliefalls <natalie.falls.down@gmail.com>
2023-03-21 15:17:39 -0400

    initalizing Pwm in module

--- src/modules/src/module.cpp
+++ src/modules/src/module.cpp
@@ -14,6 +14,18 @@ int main() {
 
     I2C_Module module(0x01, PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, parse_address(0x01));
 
+    uint pwm_in[6] = {
+		   PWM_IN_SIDE_1,
+		   PWM_IN_SIDE_2,
+		   PWM_IN_SIDE_3,
+		   PWM_IN_SIDE_4,
+		   PWM_IN_SIDE_5,
+		   PWM_IN_SIDE_6
+    };
+    uint pwm_out = PWM_OUT_PIN;
+
+    Pwm module_pwm(pwm_out, pwm_in, 6);
+
     module.setup();
 
     while(1) {
