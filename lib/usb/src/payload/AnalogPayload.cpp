91fb74a8a9c4ede10ae003f2082413dd8fda2990
wevfreeman <wevfreeman@gmail.com>
2023-04-09 01:18:42 -0400

    adding tolerance to analog/joystick payloads

--- lib/usb/src/payload/AnalogPayload.cpp
+++ lib/usb/src/payload/AnalogPayload.cpp
@@ -8,7 +8,13 @@ bool AnalogPayload::isEquivalent(const IPayload *other) const {
     if (other == nullptr) return false;
     // we know the type of other and this are the same from the precondition
     analog_data_t otherAnalog = other->toPayload().analog;
-    return (this->data.analog == otherAnalog.analog);
+    return this->isWithinTolerance(this->data.analog, otherAnalog.analog);
+}
+
+bool AnalogPayload::isWithinTolerance(uint8_t value1, uint8_t value2) const {
+    return (value1 >= value2) ?
+        (value1 - value2 <= AnalogPayload::TOLERANCE) :
+        (value2 - value1 <= AnalogPayload::TOLERANCE);
 }
 
 report_id_t AnalogPayload::reportID() const {
